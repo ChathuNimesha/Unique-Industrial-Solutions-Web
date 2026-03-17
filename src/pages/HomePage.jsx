756d58881a2a1f83fdbdf97926ff1daa2a2ba1c6
ChathuNimesha <chathurinimesha386@gmail.com>
2026-03-17 13:14:49 +0530

    remove Latest product

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,6 @@ const HomePage = () => {
             <Hero />
             <About />
             <Services />
-            <LatestProducts />
             <Industries />
             <Acheivements/>
             <Sustainability />
