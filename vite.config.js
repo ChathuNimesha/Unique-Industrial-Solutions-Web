8fb055829f15fdb97a073a840df24e138981fd75
ChathuNimesha <chathurinimesha386@gmail.com>
2026-03-28 10:19:22 +0530

    fx404

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This fixes the asset 404s on GitHub Pages by using relative paths
 })
